49d339838d8574076c49c782f0a75fb780365c61
mlundh <mlundh@gmail.com>
2014-03-20 14:48:10 +0100

    Add Copyright and GNU Public License

--- QuadFC/src/satellite_receiver_task.c
+++ QuadFC/src/satellite_receiver_task.c
@@ -1,9 +1,23 @@
 /*----------------------------------------------------------------------
  * receiver_task.c
  *
- * Created: 2013-03-24 14:50:31
- *  Author: Martin Lundh
- * ----------------------------------------------------------------------
+ * QuadFC - Firmware for a quadcopter.
+ * Copyright (C) 2014  Martin Lundh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+/* ----------------------------------------------------------------------
  * Spectrum satellite receiver task.
  * ----------------------------------------------------------------------
  * Code to parse the serial data received from the spectrum 
